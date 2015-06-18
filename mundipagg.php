146c551a0c02f10094e2bed1cb97dfeb894eef08
Matheus Moreira <mvrmoreira@gmail.com>
2015-06-18 16:17:09 -0300

    Criando script de integração com a Mundi

--- mundipagg.php
+++ mundipagg.php
@@ -1,7 +1,63 @@
 <?php
 
-$response = array();
+try
+{
+    // Carrega dependências
+    require_once(dirname(__FILE__) . '/vendor/autoload.php');
 
-http_response_code(404);
-header('Content-Type: application/json');
-print json_encode($response);
+    // Define o ambiente utilizado (produção ou homologação)
+    \MundiPagg\ApiClient::setEnvironment(\MundiPagg\One\DataContract\Enum\ApiEnvironmentEnum::STAGING);
+
+    // Define a chave da loja
+    \MundiPagg\ApiClient::setMerchantKey("be43cb17-3637-44d0-a45e-d68aaee29f47");
+
+    // Cria objeto requisição
+    $createSaleRequest = new \MundiPagg\One\DataContract\Request\CreateSaleRequest();
+
+    /**
+     * Regras do simulador:
+     * R$ 1.000,00 = Autorizada
+     * R$ 1.050,01 = Timeout
+     * R$ 1.500,00 = Não autorizada
+     */
+
+    // Define dados do pedido
+    $createSaleRequest->addCreditCardTransaction()
+        ->setPaymentMethodCode(\MundiPagg\One\DataContract\Enum\PaymentMethodEnum::SIMULATOR)
+        ->setAmountInCents(150000)
+        ->getCreditCard()
+            ->setCreditCardBrand(\MundiPagg\One\DataContract\Enum\CreditCardBrandEnum::MASTERCARD)
+            ->setCreditCardNumber("5555444433332222")
+            ->setExpMonth(12)
+            ->setExpYear(2030)
+            ->setHolderName("MUNDIPAGG")
+            ->setSecurityCode("999")
+        ;
+
+    //Cria um objeto ApiClient
+    $apiClient = new MundiPagg\ApiClient();
+
+    // Faz a chamada para criação
+    $createSaleResponse = $apiClient->createSale($createSaleRequest);
+
+    // Mapeia resposta
+    $httpStatusCode = $createSaleResponse->CreditCardTransactionResultCollection[0]->Success ? 201 : 401;
+    $response = array("message" => $createSaleResponse->CreditCardTransactionResultCollection[0]->AcquirerMessage);
+}
+catch (\MundiPagg\One\DataContract\Report\ApiError $error)
+{
+    $httpStatusCode = $error->errorCollection->ErrorItemCollection[0]->ErrorCode;
+    $response = array("message" => $error->errorCollection->ErrorItemCollection[0]->Description);
+}
+catch (\Exception $ex)
+{
+    $httpStatusCode = 500;
+    $response = array("message" => "Ocorreu um erro inesperado.");
+}
+finally
+{
+    // Devolve resposta
+    http_response_code($httpStatusCode);
+    header('Content-Type: application/json');
+    print json_encode($response);
+}
